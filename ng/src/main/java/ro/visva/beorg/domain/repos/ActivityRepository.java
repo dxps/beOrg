b80db924d80e707c87df8813e844179af1a52694
visvadw <visvadw@gmail.com>
2016-03-13 17:10:45 +0200

    Activity and Task repositories skeleton.

--- ng/src/main/java/ro/visva/beorg/domain/repos/ActivityRepository.java
+++ ng/src/main/java/ro/visva/beorg/domain/repos/ActivityRepository.java
@@ -1,11 +1,22 @@
 package ro.visva.beorg.domain.repos;
 
+import ro.visva.beorg.domain.activities.Activity;
+import ro.visva.beorg.domain.activities.Task;
+
+import java.util.List;
+
 /**
  * <br><br>
  * 2016-03-13 16:47 | visvadw | Creation time.
  */
 public interface ActivityRepository extends BaseRepository {
 
+    boolean add(Activity activity);
 
+    boolean update(Activity activity);
+
+    boolean delete(Activity activity);
+
+    List<Activity> listByTask(Task task);
 
 }
---
+++ ng/src/main/java/ro/visva/beorg/domain/repos/TaskRepository.java
@@ -0,0 +1,23 @@
+package ro.visva.beorg.domain.repos;
+
+import ro.visva.beorg.domain.activities.Task;
+import ro.visva.beorg.domain.plans.Plan;
+
+import java.util.List;
+
+/**
+ * <br><br>
+ * 2016-03-13 17:08 | visvadw | Creation time.
+ */
+public interface TaskRepository {
+
+    boolean add(Task task);
+
+    boolean update(Task task);
+
+    boolean delete(Task task);
+
+    /** List all the tasks associated with a plan. */
+    List<Task> listByPlan(Plan plan);
+
+}
