5cd0d3ad432dd11d0f1758c98b08ee928f1ff157
visvadw <visvadw@gmail.com>
2016-10-11 23:56:00 +0300

    cosmetrics only

--- ui/app.js
+++ ui/app.js
@@ -1,27 +1,25 @@
-/*
-	App configuration
-*/
-
 define([
-	"libs/webix-jet-core/core",
-	"libs/webix-jet-core/plugins/menu",
-	"libs/webix-jet-core/plugins/skin"
-], function(
-	core, menu, skin
-){
+    "libs/webix-jet-core/core",
+    "libs/webix-jet-core/plugins/menu",
+    "libs/webix-jet-core/plugins/skin"
+], function (
+    core,
+    menu,
+    skin) {
 
-	// config
-	var app = core.create({
-		id:         "beorg-ui",
-		name:       "beOrg UI",
-		version:    "0.1.0",
-		debug:      true,
-		start:      "/top/start"
-	});
+    // the app.config
+    var app = core.create({
+        id: "beorg-ui",
+        name: "beOrg UI",
+        version: "0.1.0",
+        debug: true,
+        start: "/top/start"
+    });
 
-	app.use(menu);
+    app.use(menu);
 
-	app.use(skin);
+    app.use(skin);
 
-	return app;
+    return app;
+
 });
